dce15df3cd88b63328031eed65cb3da248e3eaa2
kenichi04 <slow_life_1407@yahoo.co.jp>
2021-05-15 14:47:38 +0900

    VelocityTracker実装

--- app/src/main/java/com/example/mytouchgesture/MainActivity.java
+++ app/src/main/java/com/example/mytouchgesture/MainActivity.java
@@ -3,53 +3,74 @@ package com.example.mytouchgesture;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GestureDetectorCompat;
 import androidx.core.view.MotionEventCompat;
+import androidx.core.view.VelocityTrackerCompat;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 
 public class MainActivity extends AppCompatActivity{
-
-    private GestureDetectorCompat mDetector;
+    private static final String DEBUG_TAG = "Velocity";
+    private VelocityTracker mVelocityTracker = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Instantiate the gesture detector with the
-        // application context and an implementation of
-        // GestureDetector.OnGestureListener
-        mDetector = new GestureDetectorCompat(this,
-                new MyGestureListener());
     }
 
     // 戻り値はイベントを消費するかの真偽値
     // イベントが消費された場合、他のハンドラでこのイベントを利用することができなくなる
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // GestureDetectorCompatインスタンスは、 MainActivityのonTouchEvent()
-        // メソッドをオーバーライドした処理でタッチイベントを取得
-        this.mDetector.onTouchEvent(event);
-        // Be sure to call the superclass implementation
-        return super.onTouchEvent(event);
+
+        int index = event.getActionIndex();
+        int action = event.getActionMasked();
+        // pointerIdはタッチにおけるポインターの識別子.ジェスチャーの中で
+        // 一意になるIDがふられる. X軸とY軸における移動速度を取得する際に使用
+        int pointerId = event.getPointerId(index);
+
+        switch(action) {
+            case MotionEvent.ACTION_DOWN:
+                if (mVelocityTracker == null) {
+                    // Retrieve a new VelocityTracker object to
+                    // watch the velocity of a motion.
+                    // VelocityTrackerのインスタンスを取得
+                    mVelocityTracker = VelocityTracker.obtain();
+                } else {
+                    // Reset the velocity tracker back to its initial state.
+                    mVelocityTracker.clear();
+                }
+                //Add a user's movement to the tracker.
+                mVelocityTracker.addMovement(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mVelocityTracker.addMovement(event);
+                // When you want to determine the velocity, call
+                // computeCurrentVelocity(). Then call getXVelocity()
+                // and getYVelocity() to retrieve the velocity for each pointer ID.
+                // 引数は速度の単位, 1を渡すとピクセル毎ミリ秒、1000を渡すとピクセル毎秒
+                mVelocityTracker.computeCurrentVelocity(1000);
+                // Log velocity of pixels per second
+                // Best practice to use VelocityTrackerCompat where possible.
+                Log.d(DEBUG_TAG, "X velocity: " +
+                        VelocityTrackerCompat.getXVelocity(mVelocityTracker,
+                                pointerId));
+                Log.d(DEBUG_TAG, "Y velocity: " +
+                        VelocityTrackerCompat.getYVelocity(mVelocityTracker,
+                                pointerId));
+                break;
+            // タッチキャンセル、タッチアップ時にはVelocityTrackerを消去
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                // Returen a VelocityTracker object back to be re-used by others.
+                mVelocityTracker.recycle();
+                mVelocityTracker = null;
+                break;
+        }
+        return true;
     }
 
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        private static final String DEBUG_TAG = "Gestures";
-
-        @Override
-        public boolean onDown(MotionEvent e) {
-            Log.d(DEBUG_TAG, "onDown: " + e.toString());
-            return true;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2,
-                               float velocityX, float velocityY) {
-            Log.d(DEBUG_TAG, "onFling: " + e1.toString()+e2.toString());
-            return true;
-        }
-    }
 }
